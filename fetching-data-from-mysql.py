51944a01dd36061a2be3b2ba4c31ea5e94ab42bd
jaanvi <junejajaanvi@gmail.com>
2023-12-30 23:24:08 -0500

    build connection with mysql and fetch data

--- fetching-data-from-mysql.py
+++ fetching-data-from-mysql.py
@@ -1,2 +1,21 @@
 #! /usr/bin/env python3
-print("test")
+import mysql.connector
+
+connection_strings = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="Secret55",
+    database="companydb"
+)
+
+cursor = connection_strings.cursor()
+
+cursor.execute("SELECT * FROM Employees")
+
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
+cursor.close()
+connection_strings.close()
+
