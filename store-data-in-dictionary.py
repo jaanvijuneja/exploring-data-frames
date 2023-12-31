bad04297dfa0c17b5d6727844e81f944225bbf27
jaanvi <junejajaanvi@gmail.com>
2023-12-31 00:42:32 -0500

    store data in dictionary

---
+++ store-data-in-dictionary.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+import mysql.connector
+
+connection_strings = mysql.connector.connect(
+    host="127.0.0.1",
+    user="root",
+    password="Secret55",
+    database="companydb"
+)
+
+cursor = connection_strings.cursor()
+
+cursor.execute("SELECT * FROM Employees")
+
+results = cursor.fetchall()
+
+column_names = [desc[0] for desc in cursor.description]
+
+dictionary_data = {}
+
+#i = 0
+#for row in results:
+#    dictionary_data[i] = dict(zip(column_names, row))
+#    i += 1
+
+
+for row in results:
+    row_dict = dict(zip(column_names, row))
+    row_id = row_dict['EmployeeID']
+    dictionary_data[row_id] = row_dict
+
+print(dictionary_data)
+
+cursor.close()
+connection_strings.close()
+
